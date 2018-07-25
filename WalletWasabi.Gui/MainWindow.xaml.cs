a0e6d9a9d7d1d5dc1db42b78befadd8482397610
Lucas Ontivero <lucasontivero@gmail.com>
2018-07-25 15:22:44 -0300

    Dequeue coins

--- WalletWasabi.Gui/MainWindow.xaml.cs
+++ WalletWasabi.Gui/MainWindow.xaml.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Markup.Xaml;
 using AvalonStudio.Extensibility;
 using AvalonStudio.Shell;
 using AvalonStudio.Shell.Controls;
 using WalletWasabi.Gui.Tabs.WalletManager;
+using WalletWasabi.Models;
 
 namespace WalletWasabi.Gui
 {
@@ -50,18 +53,38 @@ namespace WalletWasabi.Gui
 			}
 		}
 
-		private void OnClosing(object sender, CancelEventArgs e)
+		private async void OnClosing(object sender, CancelEventArgs e)
 		{
-			var mustClose = AskCloseAndDequeCoins();
-			e.Cancel = !mustClose;
-			if(mustClose)
+			var ccjClient = Global.ChaumianClient;
+			if(! ccjClient.IsRunning )
 			{
-				Closing -= OnClosing;
+				e.Cancel = true;
+				return;
 			}
+
+			var inputRegistrableRound = ccjClient.State.GetRegistrableRoundOrDefault();
+			if (inputRegistrableRound == null)
+			{
+				e.Cancel = true;
+				return;
+			}
+
+			var enqueuedCoins = inputRegistrableRound.CoinsRegistered;
+
+			if( enqueuedCoins.Count == 0)
+			{
+				e.Cancel = true;
+				return;
+			}
+
+			var mustClose = await AskCloseAndDequeCoins(enqueuedCoins);
+			e.Cancel = !mustClose;
 		}
 
-		private bool AskCloseAndDequeCoins()
+		private async Task<bool> AskCloseAndDequeCoins(IEnumerable<SmartCoin> coins)
 		{
+			var ccjClient = Global.ChaumianClient;
+ 			await ccjClient.DequeueCoinsFromMixAsync(coins.ToArray());
 			return true;
 		} 
 	}
